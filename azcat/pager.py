6f4fd86604375c3c4e54f35e5da06df411408f77
Seiya Nuta <nuta@seiya.me>
2014-04-02 19:46:39 +0900

    ignore IOError from a pager correctly
    IOError have not been raised in `p.wait()', but in `p.stdin.write(...)'.

--- azcat/pager.py
+++ azcat/pager.py
@@ -3,9 +3,9 @@ from subprocess import Popen, PIPE
 
 def pipe_to_pager (s):
     p = Popen(["less", "-R", "-"], stdin=PIPE)
-    p.stdin.write(s.encode("ascii", "ignore"))
-    p.stdin = sys.stdin
     try:
+        p.stdin.write(s.encode("ascii", "ignore"))
+        p.stdin = sys.stdin
         p.wait()
     except IOError: # this will raised after the pager existed
         pass
