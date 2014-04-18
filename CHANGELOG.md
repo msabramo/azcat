f4d205d56727eb1ae05815398fce1d8191762ec5
Seiya Nuta <nuta@seiya.me>
2014-04-18 10:07:39 +0900

    version 0.20

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+v0.20:
+  - merge PR #3 by Alessandro Pisa
+  - fix #4
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="azcat",
-    version="0.12",
+    version="0.20",
     description="A alternative to cat(1); specialized for printing files",
     long_description=long_description,
     author="Seiya Nuta",
