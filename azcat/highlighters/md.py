97924e9cfacc4b9cb5e0d963cee40590f3edc81c
Seiya Nuta <nuta@seiya.me>
2014-12-25 10:19:50 +0900

    don't change background color in formatter/md

--- azcat/highlighters/md.py
+++ azcat/highlighters/md.py
@@ -1,9 +1,7 @@
-from subprocess import check_output
 from colorama import Fore, Back
 
 def highlight (out, s):
     # get the width of a terminal
-    width = check_output(["stty", "size"]).decode("utf-8").split()[1]
     in_code_block = False
 
     for l in s.split("\n"):
@@ -17,9 +15,9 @@ def highlight (out, s):
                  in_code_block = False if in_code_block else True
          # headers
          elif l.lstrip().startswith("#"):
-             out.write(bytes(Back.BLACK + Fore.WHITE, "utf-8"))
-             out.write(bytes(("{0:<" + width + "}").format(l), "utf-8"))
-             out.write(bytes(Fore.RESET + Back.RESET, "utf-8"))
+             out.write(bytes(Fore.CYAN, "utf-8"))
+             out.write(bs)
+             out.write(bytes(Fore.RESET, "utf-8"))
          # quotation
          elif l.lstrip().startswith(">"):
              out.write(bytes(Fore.BLUE, "utf-8"))
